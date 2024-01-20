564263f2a473c1f21c8cb9672ee8749b13b4b15b
José-Guilherme-20 <jgscosta20@gmail.com>
2024-01-20 01:22:40 -0300

    create project

---
+++ src/App.js
@@ -0,0 +1,5 @@
+import React from "react";
+import Login from "./pages/Login";
+export default function App() {
+  return <Login></Login>;
+}
---
+++ src/pages/Login/index.js
@@ -0,0 +1,5 @@
+import React from "react";
+import "./style.css";
+export default function Login() {
+  return <h1>Welcome</h1>;
+}

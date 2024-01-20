0cb53eaaf8961c601ab9738a36df54bf0fe56afe
José-Guilherme-20 <jgscosta20@gmail.com>
2024-01-20 14:21:05 -0300

    feat: criação tela de login

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from "react";
+import "./Global.css";
 import Login from "./pages/Login";
 export default function App() {
   return <Login></Login>;
--- src/pages/Login/index.js
+++ src/pages/Login/index.js
@@ -1,5 +1,23 @@
 import React from "react";
 import "./style.css";
+import logoImage from "../../assets/logo.svg";
+import padlock from "../../assets/padlock.png";
 export default function Login() {
-  return <h1>Welcome</h1>;
+  return (
+    <div className="login-container">
+      <section className="form">
+        <img src={logoImage} alt="Erudio logo"></img>
+        <form>
+          <h1>Acess your Account</h1>
+          <input placeholder="Username"></input>
+          <input type="password" placeholder=" Password"></input>
+
+          <button className="button" type="submit">
+            Login
+          </button>
+        </form>
+      </section>
+      <img src={padlock} alt="login"></img>
+    </div>
+  );
 }
